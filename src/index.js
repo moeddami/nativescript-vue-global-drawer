64bd37d8301ee2c640a916a458da7e63b151d5d6
Robertino Vasilescu <robertino.m@gmail.com>
2021-01-29 18:33:24 +0200

    tested and working

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import drawerMethods from './methods/drawerMethods'
 
 const NSVueGlobalDrawerPlugin = {
     install(Vue) {
-        Vue.registerElement('RadSideDrawer', () => require('nativescript-ui-sidedrawer').RadSideDrawer)
+        Vue.registerElement('RadSideDrawer', () => require('nativescript-ui-sidedrawer').RadSideDrawer);
 
         Vue.component('GlobalDrawer', NSVueGlobalDrawer)
 
